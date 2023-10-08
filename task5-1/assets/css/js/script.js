cae6ad6f9b96f5648540e345cd4263cdf07e2bf3
suzy <suzanmerhij55@gmail.com>
2023-10-09 00:38:13 +0300

    first comment

---
+++ task5-1/assets/css/js/script.js
@@ -0,0 +1,17 @@
+
+function appendCharacter(character) {
+  document.getElementById("result").value += character;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function deleteLastCharacter() {
+  var result = document.getElementById("result").value;
+  document.getElementById("result").value = result.slice(0, -1);
+}
+function calculate() {
+  var result = document.getElementById("result");
+ result.value=eval(result.value);
+}
